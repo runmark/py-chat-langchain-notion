ec2e2f18331509dc3449e83bb077a802a145c1da
Ryan Mark <ma_xingmin@163.com>
2023-03-27 18:22:18 +0800

    ✨ feat(ingest_data): ingest data complete

--- ingest_data.py
+++ ingest_data.py
@@ -1,23 +1,42 @@
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.document_loaders import NotionDirectoryLoader
-from langchain.vectorstores.faiss import FAISS
+from pathlib import Path
+from langchain.text_splitter import CharacterTextSplitter
+import faiss
+from langchain.vectorstores import FAISS
 from langchain.embeddings import OpenAIEmbeddings
 import pickle
 
-# Load Data
-loader = NotionDirectoryLoader("Notion_DB")
-raw_documents = loader.load()
+# Load data
+ps = list(Path("Notion_DB").glob("**/*.md"))
 
-# Split text
-text_splitter = RecursiveCharacterTextSplitter()
-documents = text_splitter.split_documents(raw_documents)
+data = []
+sources = []
+
+for p in ps:
+    with open(p, "r") as f:
+        data.append(f.read())
+    sources.append(p)
+
+# Split data
+# Here we split the documents, as needed, into smaller chunks.
+# We do this due to the context limits of the LLMs.
+text_splitter = CharacterTextSplitter(
+    chunk_size=1500, chunk_overlap=300, separator="\n"
+)
+
+docs = []
+metadatas = []
+
+for i, d in enumerate(data):
+    splits = text_splitter.split_text(d)
+    docs.extend(splits)
+
+    metadatas.extend([{"source": sources[i]}] * len(splits))
 
 
-# Load Data to vectorstore
-embeddings = OpenAIEmbeddings()
-vectorstore = FAISS.from_documents(documents, embeddings)
+# Create vector store from data and save it to disk
+vectorstore = FAISS.from_texts(docs, OpenAIEmbeddings(), metadatas=metadatas)
+faiss.write_index(vectorstore.index, "docs.index")
+vectorstore.index = None
 
-
-# Save vectorstore
 with open("vectorstore.pkl", "wb") as f:
     pickle.dump(vectorstore, f)
