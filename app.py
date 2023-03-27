e06f0cc9ea2655809592306ee66056d7bc11245f
Ryan Mark <ma_xingmin@163.com>
2023-03-27 22:16:23 +0800

    ✨ feat(query data): query data

--- app.py
+++ app.py
@@ -1,17 +1,49 @@
+"""Python file to serve as the frontend"""
+import streamlit as st
+from streamlit_chat import message
+import faiss
+from langchain import OpenAI
+from langchain.chains import VectorDBQAWithSourcesChain
 import pickle
-from query_data import get_chain
+
+# Load the LangChain.
+index = faiss.read_index("docs.index")
+
+with open("vectorstore.pkl", "rb") as f:
+    store = pickle.load(f)
+
+store.index = index
+chain = VectorDBQAWithSourcesChain.from_llm(
+    llm=OpenAI(temperature=0), vectorstore=store
+)
 
 
-if __name__ == "__main__":
-    with open("vectorstore.pkl", "rb") as f:
-        vectorstore = pickle.load(f)
-    qa_chain = get_chain(vectorstore)
-    chat_history = []
-    print("Chat with your docs!")
-    while True:
-        print("Human:")
-        question = input()
-        result = qa_chain({"question": question, "chat_history": chat_history})
-        chat_history.append((question, result["answer"]))
-        print("AI:")
-        print(result["answer"])
+# From here down is all the StreamLit UI.
+st.set_page_config(page_title="Blendle Notion QA Bot", page_icon=":robot:")
+st.header("Blendle Notion QA Bot")
+
+if "generated" not in st.session_state:
+    st.session_state["generated"] = []
+
+if "past" not in st.session_state:
+    st.session_state["past"] = []
+
+
+def get_text():
+    input_text = st.text_input("You: ", "Hello, how are you?", key="input")
+    return input_text
+
+
+user_input = get_text()
+
+if user_input:
+    result = chain({"question": user_input})
+    output = f"Answer: {result['answer']}\nSources: {result['sources']}"
+
+    st.session_state.past.append(user_input)
+    st.session_state.generated.append(output)
+
+if st.session_state["generated"]:
+    for i in range(len(st.session_state["generated"]) - 1, -1, -1):
+        message(st.session_state["generated"][i], key=str(i))
+        message(st.session_state["past"][i], is_user=True, key=str(i) + "_user")
--- query_data.py
+++ query_data.py
@@ -1,35 +1,24 @@
-from langchain.prompts.prompt import PromptTemplate
-from langchain.llms import OpenAI
-from langchain.chains import ChatVectorDBChain
+"""Ask a question to the notion database."""
+import faiss
+from langchain import OpenAI
+from langchain.chains import VectorDBQAWithSourcesChain
+import pickle
+import argparse
 
-_template = """Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question.
-You can assume the question about the Blendle Employee Handbook.
+parser = argparse.ArgumentParser(description="Ask a question to the notion DB.")
+parser.add_argument("question", type=str, help="The question to ask the notion DB")
+args = parser.parse_args()
 
-Chat History:
-{chat_history}
-Follow Up Input: {question}
-Standalone question:"""
-CONDENSE_QUESTION_PROMPT = PromptTemplate.from_template(_template)
+# Load the LangChain.
+index = faiss.read_index("docs.index")
 
-template = """You are an AI assistant for answering questions about the Blendle Employee Handbook.
-You are given the following extracted parts of a long document and a question. Provide a conversational answer.
-If you don't know the answer, just say "Hmm, I'm not sure." Don't try to make up an answer.
-If the question is not about the Blendle Employee Handbook, politely inform them that you are tuned to only answer questions about the Blendle Employee Handbook.
+with open("vectorstore.pkl", "rb") as f:
+    store = pickle.load(f)
 
-Question: {question}
-=========
-{context}
-=========
-Answer in Markdown:"""
-QA_PROMPT = PromptTemplate(template=template, input_variables=["question", "context"])
-
-
-def get_chain(vectorstore):
-    llm = OpenAI(temperature=0)
-    qa_chain = ChatVectorDBChain.from_llm(
-        llm,
-        vectorstore,
-        qa_prompt=QA_PROMPT,
-        condense_question_prompt=CONDENSE_QUESTION_PROMPT,
-    )
-    return qa_chain
+store.index = index
+chain = VectorDBQAWithSourcesChain.from_llm(
+    llm=OpenAI(temperature=0), vectorstore=store
+)
+result = chain({"question": args.question})
+print(f"Answer: {result['answer']}")
+print(f"Sources: {result['sources']}")
